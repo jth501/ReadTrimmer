47ed08334f2d6414c5f221339828849ebf87aa70
internEVbiotech <lightningthief@outlook.com>
2022-11-21 01:53:19 +0100

    added paired end functionality with bugs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!usr/bin/env python3
+
 import argparse
 import sys
 import re
-import gzip 
+import gzip #detect if files are gunzip - uncomplete
 import datetime
 import string
 
@@ -20,17 +21,27 @@ def parserfunc():
 
     #execute the parser method
     #parameters here are currenty tests but can be rewritten to otger file for testing
-    args = my_parser.parse_args("-f testfile.txt ".split())
+    args = my_parser.parse_args("-f testfile.txt testfile2.txt".split())
    
     return args
     
 def decompress(textwrapper):
-    for i in textwrapper:    
+    """ check if files are fasta files - returns false and exits if not
+        if true files are checked for compressiion status and decompressed if necessary
+        returns 
+    """
+    fqcheck = (".fq", ".fastq", ".fq.gz", ".fastq.gz",".txt") #remove testcase txt
+    for i in textwrapper:
+        if i.endswith(fqcheck):
+            print("file is in fastq format")
+        else:
+             return False
         checkcompress = re.search(r"\w+.g[un]z[ip]",i)
-       # checkcompress = re.search(r"\w+.txt",i) ##test case
-        if checkcompress is not None:
-            print("is a zipped file")
-            return textwrapper
+        # checkcompress = re.search(r"\w+.txt",i) ##test case
+        if checkcompress is None:
+            print("not a zipped file")
+        else:
+            return True
 
 def dict_creation(dict_file):
     """To create a dictionary from the infile data"""
@@ -65,45 +76,45 @@ def guess_encoding(id_line):
     
     
 def translation_scores(quality_line, phred_dict, encoding_dict):
-    """Function to translates Ascii characters into scores"""
+    """Function to translates Ascii characters into scores
+        returns list of scores
+    """
     quality_scores = list()
-    #To translate characters into scores
-    for char in quality_line:
-        for key,val in phred_dict.items():
-            if val[encoding_dict] == char:                   
-                quality_scores.append(int(key))     
+    #To translate characters into scores fpr
+    for qualscore in quality_line:
+        score = []
+        for char in qualscore:
+            for key,val in phred_dict.items():
+                if val[encoding_dict] == char:                   
+                    score.append(int(key))
+        quality_scores.append(score)  
+        score = []
     return quality_scores
 
 def lefttrim(read, leading, quality_scores, quality_line, window_size, quality_threshold):
     """ remove X nucleotides from 5 prime end
         return trimmed 5 prime
     """
-    try:
-        read = str(iter(read))
-        print(read)
-        # Remove X nucleotides
-        ltrim = read[leading:]
-        print(ltrim)
-        quality_scores = quality_scores[leading:]
-        quality_line = quality_line[leading:]
-        
-        # To calculate the average of a windows from 5'       
+    # Remove X nucleotides
+    ltrim = read[leading:]
+    quality_scores = quality_scores[leading:]
+    quality_line = quality_line[leading:]
+    
+    # To calculate the average of a windows from 5'       
+    quality_average = 0
+    for value in range(len(quality_scores)-(window_size-1)):  
+        for i in range(window_size):  
+            quality_average += quality_scores[value+i]      
+        result = quality_average /window_size
         quality_average = 0
-        for value in range(len(quality_scores)-(window_size-1)):  
-            for i in range(window_size):  
-                quality_average += quality_scores[value+i]      
-            result = quality_average /window_size
-            quality_average = 0
-            
-            # To trim window until finding the FIRST ONE with good quality
-            if result >= quality_threshold:
-                quality_line = quality_line[value:]
-                quality_scores = quality_scores[value:]    
-                trimmed_seq = ltrim[value:]
-                break
-            next(read)
-    except StopIteration:
-        return trimmed_seq,quality_scores, quality_line
+        
+        # To trim window until finding the FIRST ONE with good quality
+        if result >= quality_threshold:
+            quality_line = quality_line[value:]
+            quality_scores = quality_scores[value:]    
+            trimmed_seq = ltrim[value:]
+            break
+    return trimmed_seq,quality_scores, quality_line
     
 def righttrim(read, trailing, quality_scores, quality_line,window_size,quality_threshold):
     """ remove X nucleotides from 3 prime end
@@ -133,30 +144,71 @@ def righttrim(read, trailing, quality_scores, quality_line,window_size,quality_t
     return trimmed_seq,quality_scores, quality_line
 
 def checklen(trimmed, minlen):
-    if len(trimmed) > minlen:
-        return trimmed
-    else: 
-        pass # write index to log file
+    """ checks the length of the trimmed read
+        returns read of greater than minimum length
+    """
+    
+    for seq in trimmed:
+        if len(seq) > minlen:
+            return trimmed
+        else: 
+            pass # write index to log file
         
 def meanquality(lenchecked, qualityscore, qualitythreshold):
-    """check the meanquality of the trimmed read"""
+    """ calculates average qulaity of all the scores from a read
+        returns read if average is greater than quality threshold level
+    """
     sumquality = 0
     for i in qualityscore:
-        sumquality += i
+        sumquality += int(i)
     meanquality = int(sumquality/(len(qualityscore)))
+    print(meanquality)
     if meanquality > qualitythreshold:
-        return lenchecked
+        return True
+    return False
     
+def pairedend(read, leading, trailing, qualityscores, windowsize, qualthresh):
+    forward = read[0][leading:]
+    reverse = read[1][:-trailing]
+    #cut from left and right at the same time until trim stops - should be equal
     
+    #trim forward read from the 5 prime side
+    trimmed = []
+    for value in range(0,len(qualityscores[0])):
+        if sum(qualityscores[0][value:value+3])/3 > qualthresh:
+            trimmedf = forward
+            break
+        else:
+            trimmedf = forward[value:]
+    trimmed.append(trimmedf)
+    
+    #trim reverse read from the 5 prime side
+    for value in range(0,len(qualityscores[1])):
+        if sum(qualityscores[1][-(value+3):])/3 > qualthresh:
+            trimmedr = reverse
+            break
+        else:
+            trimmedr = reverse[:-(value)]
+    trimmed.append(trimmedr)
+
+    #check if reads are the same length to discard later
+    if len(trimmedf) != len(trimmedr):
+        trimmed.append(False)
+    return trimmed
+
 def run():
     """Location to run all the functions, open the file from parser
     """
     start = parserfunc()
     phred_dict = dict_creation('coding_keys.txt')
     try:
+        #check if file is fastq and if compressed change open function
         starter = decompress(start.files)
-        open_opt = gzip.open if starter is not None else open
-        #checks number of files provided and makes TextWrapper list to iterate through belpow
+        if starter is False:
+            sys.exit("The file is not in fastq format \nOnly .fq, .fastq, .fq.gz, .fastq.gz format accepted")
+        open_opt = gzip.open if starter is True else open
+        
+        #checks number of files provided and makes list to iterate through them
         if len(start.files) == 2:
             fastq = [open_opt(start.files[0], "r"), open_opt(start.files[1], "r")]
             size, num = (4 ,1)
@@ -171,36 +223,98 @@ def run():
         logfile = open("log_file.txt", "w")
         read = []
 
-        #check if fastq format in first read and exit if so - unfinished
+        # Counter to keep track of trimmed and removed reads
+        trimmed_reads = 0
+        removed_reads = 0
+        
+        #Counter to keep track of statistics
+        number_entries = 0
+        number_A = 0
+        number_C = 0
+        number_T = 0
+        number_G = 0
+        length_entries = list()
+
+        #map reads together if paired - else read single read
         for line in map(list,zip(fastq[0],fastq[num])):
             line = [x.strip() for x in line]
             read.append(line)
             #read length is 2 for single but 4 for paired
             if len(read) == size:
+                print(read)
                 if size == 2:
                     read = ["".join(x) for i in read for x in i]
                 if size == 4:
                     pass
+                number_entries += 1
+                length_entries.append(len(read[1]))
+
+                #still need to fix ability for functions to read two reads at once
+                initial_read = read[1]
+                number_A = initial_read.count('A')
+                number_C = initial_read.count('C')
+                number_T = initial_read.count('T')
+                number_G = initial_read.count('G')
+
                 dictionary = guess_encoding(read[0])
-                quality_coversion = translation_scores(read[3], phred_dict, dictionary)
-                completeleft = lefttrim(read[1], 8,quality_coversion, read[3], start.slidingwindow, start.startcut ) # third parameter will be from arg parse
-                completetrim = righttrim(completeleft[0], 8, completeleft[1],completeleft[2], start.slidingwindow, start.endcut )
-                if len(completetrim[0]) != len(completetrim[1]):
-                    sys.exit("Error - sequence length doesn't equal quality length")
-                if checklen(completetrim[0], minlen=50) is not None:
-                    pass
-                if meanquality(completetrim[0],completetrim[1],start.qualitythreshold) is not None:
+                #convert ascii values for each read/single read to decimal value
+                quality_coversion = translation_scores(read[3], phred_dict, dictionary) #returns list of decimal score
+                print(quality_coversion)
+                #take paired read
+                if len(read[1]) == 2:
+                    completetrim = pairedend(read[1], start.startcut, start.endcut,quality_coversion,start.slidingwindow,start.qualitythreshold)
+                    print(completetrim)
+                elif len(read[1]) == 1:
+                    completeleft = lefttrim(read[1],start.startcut,quality_coversion, read[3], start.slidingwindow, start.qualitythreshol ) # third parameter will be from arg parse
+                    completetrim = righttrim(completeleft[0], start.endcut, completeleft[1],completeleft[2], start.slidingwindow,start.qualitythreshold )
+                    print(completetrim)
+                    if len(completetrim[0]) != len(completetrim[1]):
+                        sys.exit("Error - sequence length doesn't equal quality length")
+                else:
+                    sys.exit("there is an error in file processing, try again")
+            
+                if completetrim == None:
+                    print("The reads could not be processed")
                     
-                    outfile.write("{0}\n\n{1}\n".format(completetrim[0], completetrim[2]))
+                #To check if the read has been trimmed or removed
+                if len(completetrim[0]) != len(initial_read):
+                    trimmed_reads += 1
+                if meanquality(completetrim[0],quality_coversion[0][:len(completetrim[0])],qualitythreshold=30) or checklen(completetrim[0], minlen=50) is None:
+                    print("stuff")
+                    removed_reads += 1
+                else:
+                    outfile.write("{0}\n{1}\n{2}\n{3}\n".format(read[0],completetrim[0], read[2],read[3][:len(completetrim[0])]))
                 read = []
+        # To put the results in a log file
         now = datetime.datetime.now()
-        # logfile.write(str(now)\t)
-        logfile.write('All reads from this FILE are CORRECTLY TRIMMED!')
+        print((str(now)), '\t','All reads from this FILE were CORRECTLY TRIMMED!', file = logfile)
+        print('Number of trimmed reads:\t', trimmed_reads, file = logfile )
+        print('Number of removed reads:\t', removed_reads, file = logfile)
+        print('\nSTATISTICS (file without trimming):', file = logfile )
+
+        print('Number of entries:\t',number_entries, file = logfile )
+        print('Number of each bases:\t', file = logfile )
+        print('\tNumber A:\t',number_A, file = logfile )
+        print('\tNumber C:\t',number_C, file = logfile )
+        print('\tNumber T:\t',number_T, file = logfile )
+        print('\tNumber G:\t',number_G, file = logfile )
+
+        print('Length of each entry:\t', file = logfile )
+        for i in range(1,number_entries+1):
+            print('\tEntry\t', i, ':\t', length_entries[i-1], file = logfile)
+
+        print('Average length of entries:\t', file = logfile )
+        print('Quality average length of entries:\t', file = logfile )
+        print('Best 10% quality entries:\t', file = logfile )
+        print('Worst 10% quality entries:\t', file = logfile )
 
     except FileNotFoundError as e:
-        print("file not found", e)  
-        
-  
+        print("file not found", e) 
+        # To put the results in a log file
+        now = datetime.datetime.now()
+        print((str(now)), '\t','ERROR: File NOT found!!', file = logfile)
+
+
 if __name__ == "__main__":
     run()
 else:
